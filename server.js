7cf3c33572da125433418aa73655a88a08aa85a3
WaffiqA <waffiq.a2002@gmail.com>
2023-04-27 15:26:38 +0700

    reorganize sturcture

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile('./sound-board/index.html');
+app.use(express.static(path.join(__dirname, '/')));
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
-	app.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}...`);
 });
