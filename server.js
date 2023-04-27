ea5ad3eea0fee537765895545cbefedc163d7de8
Waffiq Aziz <59906896+waffiqaziz@users.noreply.github.com>
2023-04-27 12:47:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html');
+  res.sendFile('./sound-board/index.html');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
